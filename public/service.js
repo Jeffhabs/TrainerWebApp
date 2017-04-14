f255ba20c6188ec297fc55acc206c2d4ae5baa86
Jeff Haberle <jeffrey.haberle@gmail.com>
2017-04-14 10:12:21 -0600

    update service

--- public/service.js
+++ public/service.js
@@ -1,7 +1,7 @@
 angular.module("MyFitnessApp").service("ClientService", function ($http, $httpParamSerializer) {
 
   //var url = "http://localhost:8080";
-  var url = "https://sheltered-fortress-22011.herokuapp.com";
+  var url = "https://desolate-oasis-69696.herokuapp.com/"
 
   var clientList = [];
   var workoutList = [];
